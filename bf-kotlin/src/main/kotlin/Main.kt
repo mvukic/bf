59306a4b078786b2a516a3990976d079c53af523
mvukic <matija.vukic2@gmail.com>
2024-09-01 18:51:54 +0200

    implement Arguments reader class

--- bf-kotlin/src/main/kotlin/Main.kt
+++ bf-kotlin/src/main/kotlin/Main.kt
@@ -2,18 +2,15 @@ package com.github.mvukic
 
 import com.github.mvukic.interpreter.Interpreter
 import com.github.mvukic.memory.Memory
-import com.github.mvukic.memory.getMemoryPath
 import com.github.mvukic.memory.readMemoryFromPath
 import com.github.mvukic.printer.ConsolePrinter
 import com.github.mvukic.program.Program
-import com.github.mvukic.program.getProgramPath
-import com.github.mvukic.program.readProgramFromPath
 
 fun main(args: Array<String>) {
-    val program = Program(readProgramFromPath(getProgramPath(args)))
-    val memory = Memory(getMemoryPath(args)?.let { readMemoryFromPath(it) } ?: byteArrayOf(1, 0))
+    val arguments = Arguments.parse(args)
+    val program = Program(arguments.programPath)
+    val memory = Memory(arguments.memoryPath?.let { readMemoryFromPath(it) } ?: ByteArray(10))
     val interpreter = Interpreter(program, memory, printer = ConsolePrinter())
-//    val interpreter = InterpreterExamples.EXAMPLE_10.asInterpreter()
     interpreter.start()
     println("Memory: ${interpreter.dumpMemory()}")
     println("Output: ${interpreter.printer.get()}")
